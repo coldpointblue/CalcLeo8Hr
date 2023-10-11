45a571626447523453ea82549db97703c8d450f7
coldpointblue <stayatplay@gmail.com>
2023-10-11 08:50:19 +0200

    Add button grid layout and button look.

--- CalcLeo8Hr/ContentView.swift
+++ CalcLeo8Hr/ContentView.swift
@@ -36,6 +36,78 @@ struct ContentView: View {
     }
 }
 
+/// Grid of calculator buttons
+struct CalculatorButtonsView: View {
+    @Binding var displayValue: String
+    var geometry: (width: CGFloat, height: CGFloat)
+    
+    let buttons: [[CalculatorButton]] = [
+        [.clear, .negate, .bitcoin, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.decimalPoint, .zero, .equals]
+    ]
+    
+    // Define row and column colors as dictionaries
+    private let rowColors: [Int: Color] = [0: CalcColor.utility]
+    private let columnColors: [Int: Color] = [3: CalcColor.operation]
+    private let overrideButtonColor: [CalculatorButton: Color] = [.equals: .orange]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ForEach(buttons.indices, id: \.self) { rowIndex in
+                HStack(spacing: 0) {
+                    ForEach(buttons[rowIndex].indices, id: \.self) { columnIndex in
+                        let button = buttons[rowIndex][columnIndex]
+                        
+                        CalculatorButtonView(
+                            button: button,
+                            displayValue: $displayValue,
+                            backgroundColor: overrideButtonColor[button] ??
+                            columnColors[columnIndex] ?? rowColors[rowIndex] ??
+                            CalcColor.digit
+                        )
+                        .frame(
+                            width: geometry.width / CGFloat(buttons[rowIndex].count),
+                            height: geometry.height / CGFloat(buttons.count)
+                        )
+                    }
+                }
+            }
+        }
+        .background(CalcColor.outside)
+    }
+}
+
+/// Individual calculator button
+struct CalculatorButtonView: View {
+    let button: CalculatorButton
+    @Binding var displayValue: String
+    let backgroundColor: Color
+    
+    var body: some View {
+        GeometryReader { geo in
+            Button(action: {
+                displayValue = button.rawValue
+            }) {
+                ZStack {
+                    backgroundColor
+                    Text(button.rawValue)
+                        .font(.system(size: 40))
+                        .foregroundColor(CalcColor.symbol)
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            .border(CalcColor.buttonBorder, width: 1)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
 #Preview {
     ContentView()
 }
