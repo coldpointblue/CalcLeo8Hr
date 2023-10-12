db62e1aecb3bba7cc4dcc8c6f21c2ce118e4cec3
coldpointblue <stayatplay@gmail.com>
2023-10-12 14:41:04 +0200

    Define details of CalculatorButton enum.

--- CalcLeo8Hr/ContentView.swift
+++ CalcLeo8Hr/ContentView.swift
@@ -1,13 +1,78 @@
 import SwiftUI
 
-/// Enum representing calculator button labels.
-enum CalculatorButton: String, CaseIterable {
-    case clear = "AC", negate = "±", bitcoin = "₿", divide = "÷",
-         seven = "7", eight = "8", nine = "9", multiply = "×",
-         four = "4", five = "5", six = "6", subtract = "-",
-         one = "1", two = "2", three = "3", add = "+",
-         sine = "sin", cosine = "cos",
-         zero = "0", decimalPoint = ".", equals = "="
+/// Enum representing calculator button labels
+enum CalculatorButton: CustomStringConvertible, Hashable {
+    
+    // Types of buttons
+    case operation(Operation) // Note: Operation enum separate, DRY
+    case standard(Standard)
+    case digit(Digit)
+    
+    // MARK: - Nested Enums
+    
+    /// Enumerates standard buttons like AC, ±, etc.
+    enum Standard: String, CaseIterable, Hashable {
+        case clear = "AC"
+        case negate = "±"
+        case bitcoin = "₿"
+        case decimalPoint = "."
+        case equal = "="
+        
+        /// Returns symbol for the standard button
+        var symbol: String { rawValue }
+    }
+    
+    /// Enumerates number buttons 0-9
+    enum Digit: String, CaseIterable, Hashable {
+        case zero = "0", one = "1", two = "2", three = "3", four = "4",
+             five = "5", six = "6", seven = "7", eight = "8", nine = "9"
+        
+        /// Returns symbol for the digit
+        var symbol: String { rawValue }
+    }
+    
+    // MARK: - Properties
+    
+    /// Returns description for specific CalculatorButton
+    var description: String {
+        switch self {
+        case .operation(let op): return op.symbol
+        case .standard(let standard): return standard.symbol
+        case .digit(let digit): return digit.symbol
+        }
+    }
+    
+    /// Returns array of all possible CalculatorButtons
+    static var allCases: [CalculatorButton] {
+        let standardButtons = Standard.allCases.map(CalculatorButton.standard)
+        let operationButtons = Operation.allCases.map(CalculatorButton.operation)
+        let digitButtons = Digit.allCases.map(CalculatorButton.digit)
+        
+        return standardButtons + operationButtons + digitButtons
+    }
+    
+    // MARK: - Hashable Conformance
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .operation(let operation): hasher.combine(operation)
+        case .standard(let standard): hasher.combine(standard)
+        case .digit(let digit): hasher.combine(digit)
+        }
+    }
+    
+    static func == (lhs: CalculatorButton, rhs: CalculatorButton) -> Bool {
+        switch (lhs, rhs) {
+        case (.operation(let leftOperation), .operation(let rightOperation)):
+            return leftOperation == rightOperation
+        case (.standard(let leftStandard), .standard(let rightStandard)):
+            return leftStandard == rightStandard
+        case (.digit(let leftDigit), .digit(let rightDigit)):
+            return leftDigit == rightDigit
+        default:
+            return false
+        }
+    }
 }
 
 /// Struct for calculator color scheme.
