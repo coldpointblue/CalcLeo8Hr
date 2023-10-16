a2459dd9df696cd5f730b75bbfb2c9b5e339d1e6
coldpointblue <stayatplay@gmail.com>
2023-10-16 10:56:36 +0200

    Added an alternate set of colors.

--- CalcLeo8Hr/Views/CalcColor.swift
+++ CalcLeo8Hr/Views/CalcColor.swift
@@ -2,16 +2,23 @@ import SwiftUI
 
 /// Struct for calculator color scheme
 struct CalcColor {
-    static let display = Color.white
-    static let outside = Color.black
-    static let utility = Color(.systemGray)
-    static let digit = Color(.systemGray2)
-    static let operation = Color.orange
-    static let buttonBorder = Color.black
-    static let symbol = Color.white
+    enum ColorScheme {
+        case `default`
+        case alternate
+    }
+    
+    static var activeColorScheme: ColorScheme = .default
+    
+    static var display: Color { activeColorScheme == .alternate ? .black : .white }
+    static var outside: Color { activeColorScheme == .alternate ? .white : .black }
+    static var utility: Color { activeColorScheme == .alternate ? Color(.systemGray5) : Color(.systemGray) }
+    static var digit: Color { activeColorScheme == .alternate ? Color(.systemGray4) : Color(.systemGray2) }
+    static var operation: Color { activeColorScheme == .alternate ? .blue : .orange }
+    static var buttonBorder: Color { activeColorScheme == .alternate ? .white : .black }
+    static var symbol: Color { activeColorScheme == .alternate ? .black : .white }
     
     /// Returns display color based on the orientation to fill outside
     static func edgeDisplay(for orientation: CGSize) -> Color {
-        orientation.width > orientation.height ? outside : display
+        return orientation.width > orientation.height ? outside : display
     }
 }
