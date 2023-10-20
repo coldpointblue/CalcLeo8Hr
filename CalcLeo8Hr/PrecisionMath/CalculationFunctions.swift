847a5ba7b2d28272588ab554a78fc07d59b031ae
coldpointblue <stayatplay@gmail.com>
2023-10-20 13:05:12 +0200

    Adapt CalculatorFuctions to Decimal.

--- CalcLeo8Hr/PrecisionMath/CalculationFunctions.swift
+++ CalcLeo8Hr/PrecisionMath/CalculationFunctions.swift
@@ -3,18 +3,18 @@ import Foundation
 // MARK: - Core Calculation Functions
 
 struct CalculationFunctions {
-    static let fixedDecimals = 16
-    static let decimalPlaces : Decimal = Decimal(sign: .plus, exponent:  CalculationFunctions.fixedDecimals, significand: Decimal(10))
+    static var fixedDecimals = 16 // Indicate the precision level
+    static var decimalPlaces: Decimal { pow(10, fixedDecimals) }
     
     /// Compute precise percent change between two numbers given as strings
-    func precisePercent(_ numerator: String, _ denominator: String) -> String {
+    static func precisePercent(_ numerator: String, _ denominator: String) -> String {
         let calculation = (preciseRatio(numerator, denominator) * 100) - 100
         let result = ((calculation * 100).wholePart) / 100
         return (result.isSignMinus ? "" : "+") + "\(result)"
     }
     
     /// Compute a precise ratio between two numbers given as strings, returning Decimal
-    func preciseRatio(_ numerator: String, _ denominator: String) -> Decimal {
+    static func preciseRatio(_ numerator: String, _ denominator: String) -> Decimal {
         guard let firstNumber = Decimal(string: numerator),
               let secondNumber = Decimal(string: denominator) else {
             return Decimal(0)
@@ -24,20 +24,52 @@ struct CalculationFunctions {
         return ((calculation * decimalPlaces).wholePart) / decimalPlaces
     }
     
-    // Compute precise division by multiplicying Reciproacl, returning String
-    func preciseDivision(_ numerator: String, _ denominator: String) -> String {
-        guard let firstNumber = Decimal(string: numerator),
-              let secondNumber = Decimal(string: denominator) else {
-            return "0.00" // Zero when numbers are not converted to Decimal
+    enum PrecisionDivisionResult {
+        case valid(Decimal, String)
+        case error(String)
+        
+        // To choose result type, throws an error if it fails. i.e. result.asTuple.0 or 1
+        func asTupleThrowingErr() throws -> (Decimal, String) {
+            if let result = createTuple() { return result }
+            throw CalculatorError.divideByZero
+        }
+        
+        var asTuple: (Decimal, String) {
+            return createTuple() ?? (Decimal.nan, "Not a Number")
+        }
+        
+        /// - Returns: Optional tuple of Decimal and String.
+        private func createTuple() -> (Decimal, String)? {
+            switch self {
+            case .valid(let decimal, let string):
+                return (decimal, string)
+            case .error(_):
+                return nil
+            }
+        }
+    }
+    
+    /// Precisely divides two Decimal numbers by multiplying Reciprocal. Returns (#, String).
+    /// - Parameters:
+    ///   - numerator: Numerator in Decimal.
+    ///   - denominator: Denominator in Decimal.
+    /// - Returns: Tuple (resultTruncated, fixedLengthString), Decimal and String.
+    static func preciseDivision(_ numerator: Decimal, _ denominator: Decimal) throws -> PrecisionDivisionResult {
+        guard denominator != 0 else {
+            throw CalculatorError.divideByZero
         }
         
         let decimalPlaces = CalculationFunctions.decimalPlaces
-        let reciprocalOfDenominator = Decimal().safeReciprocal(secondNumber)
+        let reciprocalOfDenominator = Decimal().safeReciprocal(denominator)
+        guard !reciprocalOfDenominator.isNaN else {
+            return PrecisionDivisionResult.error("Not a Number")
+        }
         
-        let calculation = (firstNumber * reciprocalOfDenominator) // Division
-        
+        let preciseCalculation = (numerator * reciprocalOfDenominator) // Division
         // Truncate to fixed decimals
-        let result = ((calculation * decimalPlaces).wholePart) / decimalPlaces
-        return result.digitsSeenPadded(CalculationFunctions.fixedDecimals)
+        let truncated = ((preciseCalculation * decimalPlaces).wholePart) / decimalPlaces
+        let resultFixedLength = truncated.digitsSeenPadded(CalculationFunctions.fixedDecimals)
+        
+        return PrecisionDivisionResult.valid(preciseCalculation, resultFixedLength)
     }
 }
