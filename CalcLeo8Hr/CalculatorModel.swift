cee98cca2a23e4bf88eb171858fb441887f84751
coldpointblue <stayatplay@gmail.com>
2023-10-20 13:08:33 +0200

    Adjust CalculatorModel to use Decimal type.

--- CalcLeo8Hr/CalculatorModel.swift
+++ CalcLeo8Hr/CalculatorModel.swift
@@ -2,46 +2,77 @@ import Foundation
 
 enum CalculatorError: Error {
     case divideByZero
+    
+    var description: String {
+        switch self {
+        case .divideByZero:
+            return "Division by zero is invalid."
+        }
+    }
 }
 
 struct CalculatorModel {
-    var currentTotal: Double = 0.0
-    var givenNumber: Double = 0.0
+    var currentTotal: Decimal = 0.0
+    var givenNumber: Decimal = 0.0
     var operation: Operation = .none
     
     /// Arithmetic performed then updates currentTotal
     mutating func performOperation() {
         do {
-            switch operation {
-            case .add:
-                currentTotal += givenNumber
-            case .subtract:
-                currentTotal -= givenNumber
-            case .multiply:
-                currentTotal *= givenNumber
-            case .divide:
-                currentTotal = try divide(dividend: currentTotal, divisor: givenNumber)
-            case .sine:
-                currentTotal = sin(givenNumber)
-            case .cosine:
-                currentTotal = cos(givenNumber)
-            case .none:
-                break
-            }
+            try calculateResult()
         } catch CalculatorError.divideByZero {
             Logger.log("Cannot divide by zero.", type: .error)
-            currentTotal = Double.nan
+            currentTotal = Decimal.nan
         } catch {
             Logger.log("An unknown error occurred.", type: .error)
         }
     }
     
-    /// Division that handles division by zero
-    func divide(dividend: Double, divisor: Double) throws -> Double {
+    private mutating func calculateResult() throws {
+        switch operation {
+        case .add:
+            currentTotal += givenNumber
+        case .subtract:
+            currentTotal -= givenNumber
+        case .multiply:
+            currentTotal *= givenNumber
+        case .divide:
+            try handleDivision()
+        case .sine:
+            currentTotal = sin(givenNumber.asDouble).asDecimal
+        case .cosine:
+            currentTotal = cos(givenNumber.asDouble).asDecimal
+        case .none:
+            break // currentTotal = givenNumber?
+        }
+    }
+    
+    private mutating func handleDivision() throws {
+        do {
+            let result = try CalculationFunctions.preciseDivision(currentTotal, givenNumber)
+            currentTotal = result.asTuple.0
+        } catch CalculatorError.divideByZero {
+            currentTotal = Decimal.nan
+        } catch {
+            throw GenericError.invalidOperation("Unknown Error within Division")
+        }
+    }
+    
+    /// Normal division that handles division by zero.
+    func divideImprecise(dividend: Decimal, divisor: Decimal) throws -> Decimal {
         guard divisor != 0.0 else {
             throw CalculatorError.divideByZero
         }
         return dividend / divisor
+    }
+    
+    mutating func negate(_ valueToNegate: ValueToNegate) {
+        switch valueToNegate {
+        case .currentTotal:
+            currentTotal = -currentTotal
+        case .givenNumber:
+            givenNumber = -givenNumber
+        }
     }
     
     mutating func reset() {
@@ -49,4 +80,9 @@ struct CalculatorModel {
         givenNumber = 0.0
         operation = .none
     }
+}
+
+enum ValueToNegate {
+    case currentTotal
+    case givenNumber
 }
